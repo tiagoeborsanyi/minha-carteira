1de5ccd64abdaeff3ee230a225f3696c32f0ce62
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-02-19 16:20:47 -0300

    create SelectInput component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import SelectInput from '../../components/SelectInput';
+import ContentHeader from '../../components/ContentHeader';
+import { Container } from './styles';
+
 const Dashboard: React.FC = () => {
+  const options = [
+    {value: 'Caio', label: 'Caio'},
+    {value: 'Marisa', label: 'Marisa'},
+    {value: 'Julio', label: 'Julio'}
+  ]
+
   return (
-    <h1>Dashboard 2</h1>
+    <Container>
+      <ContentHeader title='Dashboard' lineColor='#f793dc'>
+        <SelectInput options={options} />
+      </ContentHeader>
+    </Container>
   );
 }
 
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
+import { Container } from './styles';
+
 const List: React.FC = () => {
+  const options = [
+    {value: 'Caio', label: 'Caio'},
+    {value: 'Marisa', label: 'Marisa'},
+    {value: 'Julio', label: 'Julio'}
+  ]
+
   return (
-    <h1>List</h1>
+    <Container>
+      <ContentHeader title='Saidas' lineColor='#e44c4e'>
+        <SelectInput options={options} />
+      </ContentHeader>
+    </Container>
   );
 }
 
