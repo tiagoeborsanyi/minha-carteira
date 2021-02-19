f422b95f839fe37e6f763b4ec8aab68fdd543ab9
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-02-19 13:49:19 -0300

    create aside Bar

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { 
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp
+ } from 'react-icons/md';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { 
+  Container, 
+  Header, 
+  LogoImg,
+  Title,
+  MenuContainer, 
+  MenuItemLink 
+} from './styles';
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside 2</h1>
+      <Header>
+        <LogoImg src={logoImg} alt='Logo Minha Carteira' />
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink href='#'>
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+
+        <MenuItemLink href='#'>
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+
+        <MenuItemLink href='#'>
+          <MdArrowDownward />
+          Saidas
+        </MenuItemLink>
+
+        <MenuItemLink href='#'>
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 }
