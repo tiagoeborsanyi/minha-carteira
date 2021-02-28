d100c4615e5c960f2ad18261460cffa5f4557227
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-02-28 15:44:44 -0300

    filtered by type financial moviment

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -7,6 +7,7 @@ import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
 import formatCurrency from '../../utils/formatCurrency';
 import formatDate from '../../utils/formatDate';
+import listOfMonths from '../../utils/months';
 import { Container, Content, Filters } from './styles';
 
 interface IRouteParams {
@@ -30,6 +31,7 @@ const List: React.FC<IRouteParams> = ({ match }) => {
   const [data, setData] = useState<IData[]>([]);
   const [monthSelected, setMonthSelected] = useState<string>(String(new Date().getMonth() + 1));
   const [yearSelected, setYearSelected] = useState<string>(String(new Date().getFullYear()-1));
+  const [selectedFrequency, setSelectedFrequency] = useState(['recorrente', 'eventual']);
 
   const { type } = match.params; 
 
@@ -45,19 +47,44 @@ const List: React.FC<IRouteParams> = ({ match }) => {
     return type === 'entry-balance' ? gains : expenses;
   }, [type]);
 
-  const months = [
-    {value: 2, label: 'Fevereiro'},
-    {value: 3, label: 'Março'},
-    {value: 8, label: 'Agosto'},
-    {value: 7, label: 'Julho'},
-    {value: 9, label: 'Setembro'}
-  ]
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
 
-  const years = [
-    {value: 2020, label: 2020},
-    {value: 2019, label: 2019},
-    {value: 2018, label: 2018}
-  ]
+    listData.forEach(item => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year)
+      }
+    });
+    return uniqueYears.map(year => {
+      return {
+        value: year,
+        label: year
+      }
+    })
+  }, [listData]);
+
+  const months = useMemo(() => {
+    return listOfMonths.map((month, index) => {
+      return {
+        value: index+1,
+        label: month
+      }
+    });
+  }, []);
+
+  const handleFrequencyClick = (frequency: string) => {
+    const alreadySelected = selectedFrequency.findIndex(item => item === frequency);
+
+    if (alreadySelected>= 0) {
+      const filtered = selectedFrequency.filter(item => item != frequency);
+      setSelectedFrequency(filtered);
+    } else {
+      setSelectedFrequency(prev => [...prev, frequency])
+    }
+  }
 
   useEffect(() => {
 
@@ -70,7 +97,7 @@ const List: React.FC<IRouteParams> = ({ match }) => {
       // console.log(month, monthSelected);
       // console.log(year, date)
 
-      return month === monthSelected && year === yearSelected;
+      return month === monthSelected && year === yearSelected && selectedFrequency.includes(item.frequency);
     });
 
     const formatedData = filteredDate.map(item => {
@@ -84,7 +111,7 @@ const List: React.FC<IRouteParams> = ({ match }) => {
       }
     })
     setData(formatedData);
-  }, [listData, monthSelected, yearSelected, data.length])
+  }, [listData, monthSelected, yearSelected, data.length, selectedFrequency])
 
   return (
     <Container>
@@ -96,13 +123,15 @@ const List: React.FC<IRouteParams> = ({ match }) => {
       <Filters>
         <button 
           type='button'
-          className='tag-filter tag-filter-recurrent'
+          className={`tag-filter tag-filter-recurrent ${selectedFrequency.includes('recorrente') && 'tag-actived'}`}
+          onClick={() => handleFrequencyClick('recorrente')}
         >
           Recorrentes
         </button>
         <button 
           type='button'
-          className='tag-filter tag-filter-eventual'
+          className={`tag-filter tag-filter-eventual ${selectedFrequency.includes('eventual') && 'tag-actived'}`}
+          onClick={() => handleFrequencyClick('eventual')}
         >
           Eventuais
         </button>
