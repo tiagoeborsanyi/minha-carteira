da2c6e4b8f7247ea33fbb558dc1f6604f704d4cf
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-02-28 13:12:22 -0300

    formated dates month and year

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -5,6 +5,8 @@ import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
+import formatCurrency from '../../utils/formatCurrency';
+import formatDate from '../../utils/formatDate';
 import { Container, Content, Filters } from './styles';
 
 interface IRouteParams {
@@ -58,9 +60,9 @@ const List: React.FC<IRouteParams> = ({ match }) => {
       return {
         id: String(Math.random() * data.length + i),
         description: item.description,
-        amountFormatted: item.amount,
+        amountFormatted: formatCurrency(Number(item.amount)),
         frequency: item.frequency,
-        dataFormatted: item.date,
+        dataFormatted: formatDate(item.date),
         tagColor: item.frequency === 'recorrente' ? '#4e41f0' : '#e44c4e'
       }
     })
