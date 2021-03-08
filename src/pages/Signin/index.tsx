616e2bfc7572d70ce737127bf9f7f2452d685a50
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-08 15:38:30 -0300

    work on component input

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
+import logoImg from '../../assets/logo.svg';
+import {
+  Container,
+  Logo,
+  Form,
+  FormTitle
+} from './styles';
+
 const SignIn: React.FC = () => {
   return (
-    <h1>SignIn</h1>
+    <Container>
+      <Logo>
+        <img src={logoImg} alt='logo' />
+        <h2>Minha Carteira</h2>
+      </Logo>
+
+      <Form>
+        <FormTitle>
+          Entrar
+        </FormTitle>
+
+        <input type='text' />
+        <input type='text' />
+
+        <button type='submit'>Acessar</button>
+      </Form>
+    </Container>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 import App from './app.routes';
+import Auth from './auth.routes';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
-    <App />
+    <Auth />
   </BrowserRouter>
 )
 
