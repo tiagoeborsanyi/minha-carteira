42833eadc4c3d68f718f1eea649fe34e24ff8d48
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-08 15:15:10 -0300

    finish custom theme dark and light

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -3,15 +3,27 @@ import React from 'react';
 
 import { Container, ToggleLabel, ToggleSelector } from './styles';
 
-const Toggle: React.FC = () => (
+interface IToggleProps {
+  labelLeft: string;
+  labelRight: string;
+  checked: boolean;
+  onChange(): void;
+}
+
+const Toggle: React.FC<IToggleProps> = ({
+  labelLeft,
+  labelRight,
+  checked,
+  onChange
+}) => (
   <Container>
-    <ToggleLabel>Light</ToggleLabel>
+    <ToggleLabel>{labelLeft}</ToggleLabel>
     <ToggleSelector
-      checked
+      checked={checked}
       uncheckedIcon={false}
       checkedIcon={false}
-      onChange={() => {}} />
-    <ToggleLabel>Dark</ToggleLabel>
+      onChange={onChange} />
+    <ToggleLabel>{labelRight}</ToggleLabel>
   </Container>
 )
 
